b2d8c602ccebd2bc3da65c270e0f6ff682d6a894
meghana-8938 <meghanagonepelly@gmail.com>
2025-09-22 13:56:08 +0530

    text

---
+++ functions.js
@@ -0,0 +1,25 @@
+//functions
+// code resuability
+// function add() {
+//     var a=67;
+//     var b=89;
+//     document.write(a+b);
+//     console.log(a+b);
+
+// }
+// add();
+
+// const add=(a,b)=> console.log(a+b);
+// add(45,67);
+//hoisting = to move declaration to the top
+// console.log(b);
+// b=45;
+
+let name="john";
+
+function_demo(); {
+    let name="meghnaa";
+    document.write('hello, my name is ${name}');
+}
+   document.write('hello, my name is ${name}');
+    Demo();
