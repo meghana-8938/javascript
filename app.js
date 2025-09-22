385145f8b902777ff046b4758f3ce4e43ad5fd7f
meghana-8938 <meghanagonepelly@gmail.com>
2025-09-22 11:07:20 +0530

    app

---
+++ app.js
@@ -0,0 +1,29 @@
+
+
+//commit
+//commit used for understand the code by developer
+//data types
+//1.primitive  2.non premitive
+//primitive data types
+
+a="748";
+console.log(typeof (a));
+name="meghana";
+console.log(typeof (name));
+c=true;
+console.log(typeof (c));
+data=null;
+console.log(typeof (data));
+d=undefined;
+console.log(typeof (d))
+
+
+//non primitive data types
+array=[50,"meghana",true,null];
+console.log(typeof (array));
+data={
+    name:"meghana",
+    age:22,
+    dep:"cse",
+};
+console.log(typeof (data));
